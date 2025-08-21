7223b1f1e0f7157f9dae387ff4f2a19335280161
Bee <117178732+everloste@users.noreply.github.com>
2025-08-21 21:59:33 +0200

    accept empty as version in gui

--- src/page.ts
+++ src/page.ts
@@ -4,6 +4,6 @@ export function versionFeedback(this: HTMLInputElement) {
 	const version = this.value;
 	const feedback = this.parentNode!.querySelector("#version-input-feedback")! as HTMLSpanElement;
 
-	if (!mcverIsValid(version)) feedback.hidden = false;
+	if (!mcverIsValid(version) && version != "") feedback.hidden = false;
 	else feedback.hidden = true;
 }
